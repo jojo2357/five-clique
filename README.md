60e5277e5e9cd015e9221dd04ec4cc5c8a314ca5
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2022-08-08 16:39:20 -0700

    Move to n sizing
    Remove the redundancies and make n-clique.js able to accept command line args to enable selecting anagrams or changing the word count/size

--- README.md
+++ README.md
@@ -5,7 +5,16 @@ To run (after on local machine)
 * Download data file: `curl https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt>words_alpha.txt`
 * Run parser: `node five-clique.js`
 
+```
+Parser OPTIONS:
+includeanagrams : if present in args, will include all anagrams.
+wordsize <size> : the size of the word to use, default is 5
+wordcount <count> : the number of words to use
+outfile <filename>: the file to write results to. Writes in csv, so it is reccommended to provide a csv file.
+```
+
 Anagrams time: ~135s
+
 No anagrams time: ~50s
 
 Uses python: no
---
+++ n-clique.js
@@ -0,0 +1,170 @@
+/*
+command line usage: `node n-clique.js [OPTIONS]`
+
+OPTIONS:
+includeanagrams : if present in args, will include all anagrams.
+wordsize <size> : the size of the word to use, default is 5
+wordcount <count> : the number of words to use
+outfile <filename>: the file to write results to. Writes in csv, so it is reccommended to provide a csv file.
+ */
+
+let args = process.argv.slice(2);
+
+let options = {
+    "includeanagrams": false,
+    "wordsize": 5,
+    "wordcount": 5
+};
+
+options["outfile"] = `${options.wordsize}x${options.wordcount},${options.includeanagrams ? "anagrams": "no_anagrams"}.csv`
+
+for (let i = 0; i < args.length; i += 2) {
+    switch (args[i].toLowerCase()) {
+        case "include-anagrams":
+            i--;
+            options.includeanagrams = true;
+            break;
+        case "wordsize":
+            if (i + 1 === args.length)
+                console.error("No option supplied for wordsize");
+            else
+                options.wordsize = Number.parseInt(args[i + 1]);
+            break;
+        case "wordcount":
+            if (i + 1 === args.length)
+                console.error("No option supplied for wordcount");
+            else
+                options.wordcount = Number.parseInt(args[i + 1]);
+            break;
+        case "outfile":
+            if (i + 1 === args.length)
+                console.error("No option supplied for outfile");
+            else
+                options.outfile = Number.parseInt(args[i + 1]);
+            break;
+    }
+}
+
+if (options.wordcount * options.wordsize > 26) {
+    console.error("This program was not designed to handle overlap. Please use less words or smaller words");
+    process.exit(-1);
+}
+if (options.wordcount < 2 || options.wordsize < 2) {
+    console.error("invalid args");
+    process.exit(-1);
+}
+
+const fs = require('fs');
+
+const start = Date.now();
+
+function getTime() {
+    return Date.now() - start;
+}
+
+process.on('exit', () => console.log("Completed in", getTime(), "ms"));
+
+/**
+ * @param setA {number[]}
+ * @param setB {number[]}
+ * @return {number[]}
+ */
+function unionByFloor(setA, setB) {
+    let aDex = 0, bDex = 0;
+    let localOut = [];
+
+    while (aDex < setA.length && bDex < setB.length) {
+        if (setA[aDex] < setB[bDex])
+            aDex++;
+        else if (setA[aDex] > setB[bDex])
+            bDex++;
+        else {
+            localOut.push(setA[aDex++]);
+            bDex++;
+        }
+    }
+    return localOut;
+}
+
+process.stdout.write("Reading in data        \r");
+
+/**
+ * @type {[{neighbors: number[], word: String, chars: String[], numneighbors: number}]}
+ */
+const data = fs.readFileSync("words_alpha.txt").toString().split(/\r?\n/).reduce((prev, line) => {
+    if (line.length === options.wordsize) {
+        if (!options.includeanagrams) {
+            let myAlph = line.split("").sort().join("");
+            if (prev.every(word => word.alph !== myAlph) && line.split("").every((char, windex, arr) => arr.indexOf(char) === windex)) prev.push({
+                word: line,
+                chars: line.split("").sort(),
+                alph: myAlph,
+                neighbors: [],
+                numneighbors: 0
+            });
+        } else {
+            if (line.split("").every((char, windex, arr) => arr.indexOf(char) === windex)) prev.push({
+                word: line,
+                chars: line.split(""),
+                neighbors: [],
+                numneighbors: 0
+            });
+        }
+    }
+    return prev;
+}, []);
+
+process.stdout.write("Sorting data        \r");
+
+for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data.length; j++) {
+        if (i !== j && data[i].chars.every(char => !data[j].chars.includes(char))) {
+            ++data[i].numneighbors;
+        }
+    }
+}
+
+data.sort((a, b) => a.numneighbors - b.numneighbors);
+
+process.stdout.write("Connecting nodes        \r");
+
+for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+        if (data[i].chars.every(char => !data[j].chars.includes(char))) {
+            data[i].neighbors.push(j);
+        }
+    }
+}
+
+console.log("Created graph with", data.length , "entries.", `Took ${getTime()}ms`);
+
+fs.writeFileSync(options.outfile, "")
+
+let finds = 0;
+
+function outputResult(...res) {
+    fs.appendFileSync(options.outfile, res.reduce((previousValue, currentValue, currentIndex, array) => {
+        return previousValue + data[currentValue].word + (currentIndex !== array.length - 1 ? "," : "");
+    }, "") + "\n");
+    ++finds;
+}
+
+function recursiveFunction(startingIndex, acceptableNeighbors, depth, targetDepth, previousIndicies) {
+    let newNeighbors = unionByFloor(acceptableNeighbors, data[startingIndex].neighbors);
+    for (const newNeighbor of newNeighbors) {
+        if (depth === targetDepth - 1) {
+            outputResult(...previousIndicies, newNeighbor);
+        } else {
+            recursiveFunction(newNeighbor, newNeighbors, depth + 1, targetDepth, [...previousIndicies, newNeighbor]);
+        }
+    }
+}
+
+for (let i = 0; i < data.length; i++) {
+    if (!(i % 100))
+        console.log(i, (100 * i / data.length).toFixed(2) + "%", finds);
+    let i_sNeighbors = data[i].neighbors;
+    recursiveFunction(i, i_sNeighbors, 1, options.wordcount, [i]);
+}
+
+console.log("Found" , finds, "results");
